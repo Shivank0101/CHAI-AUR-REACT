d329be93e4df41b5d6861c69ff160830466f7ca2
Shivank Shukla <shivank01011995@gmail.com>
2025-07-18 18:13:04 +0530

    Update App.jsx in gbchnager

--- bgchnager/src/App.jsx
+++ bgchnager/src/App.jsx
@@ -4,6 +4,11 @@ import React, { useState } from 'react'
 const App = () => {
 const [color, setColor] = useState("olive")
 
+// but aise kinte function banate jaayenege 
+const changeRed = ()=>{
+  setColor('red')
+} 
+
   return (
   <div className='w-full h-screen duration-200 '
   style={{background:color}}
@@ -11,12 +16,13 @@ const [color, setColor] = useState("olive")
     <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
       <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
         <button
-        onClick={()=>setColor('red')}
+        onClick={changeRed}
         className=' rounded bg-red-500'
         >Red</button>
 
+
          <button
-        onClick={()=>setColor('green')}
+        onClick={()=>setColor('green')} // another method
         className='rounded bg-green-500'
         >Green</button>
          <button
